3e10090c26ee001ead3206fa79e6f7a0c64f7a05
choHarmony <jws0183@naver.com>
2022-12-08 15:05:58 +0900

    중앙 정렬시키기

--- Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ Unit2/Pathway2/Lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
@@ -17,12 +21,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LemonadeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                LemonadeTheme {
+                    LemonApp()
                 }
             }
         }
@@ -30,14 +30,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LemonApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Text(text = "Hello there!")
+    }
+}
+
+@Composable
+fun LemonTextAndImage(
+    textLabelId: Int,
+    drawableId: Int,
+    descriptionId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LemonadeTheme {
-        Greeting("Android")
+        LemonApp()
     }
 }
